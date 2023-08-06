a730ac95854d2f183b2648e6182f812c73bf857e
Khushi Saini <133095807+khushigaugyann@users.noreply.github.com>
2023-08-06 09:14:41 +0530

    Add files via upload

---
+++ Pattern4.java
@@ -0,0 +1,10 @@
+public class Pattern4 {
+    public static void main(String[] args) {
+        for(int i=6;i>=1;i--){
+            for(int j=6;j>=i;j--){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
